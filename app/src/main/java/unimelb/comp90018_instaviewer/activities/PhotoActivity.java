a0915346357706238214e8e5f558cc6ee7e3d24b
Rio Susanto <>
2018-09-13 13:50:32 +1000

    Setup initial android project

---
+++ app/src/main/java/unimelb/comp90018_instaviewer/activities/PhotoActivity.java
@@ -0,0 +1,15 @@
+package unimelb.comp90018_instaviewer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import unimelb.comp90018_instaviewer.R;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
---
+++ app/src/main/java/unimelb/comp90018_instaviewer/models/ExampleModel.java
@@ -0,0 +1,4 @@
+package unimelb.comp90018_instaviewer.models;
+
+public class ExampleModel {
+}
