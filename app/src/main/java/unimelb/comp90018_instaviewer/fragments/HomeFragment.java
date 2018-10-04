e4adf3f3dbba8866e20fe375477d450f69e0666e
rchkmchk <mcheng3@student.unimelb.edu.au>
2018-10-04 16:07:17 +1000

    Initiate home fragment

---
+++ app/src/main/java/unimelb/comp90018_instaviewer/fragments/HomeFragment.java
@@ -0,0 +1,20 @@
+package unimelb.comp90018_instaviewer.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.v4.app.Fragment;
+
+import unimelb.comp90018_instaviewer.R;
+
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_home, container, false);
+    }
+}
