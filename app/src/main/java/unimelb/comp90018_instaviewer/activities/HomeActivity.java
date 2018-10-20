c469685cd457822736f56c1e6a93b5c3834da96c
rchkmchk <mcheng3@student.unimelb.edu.au>
2018-10-20 14:10:16 +1100

    Set LauncherActivity as launcher, added line to confirm logged in detail

--- app/src/main/java/unimelb/comp90018_instaviewer/activities/HomeActivity.java
+++ app/src/main/java/unimelb/comp90018_instaviewer/activities/HomeActivity.java
@@ -1,12 +1,17 @@
 package unimelb.comp90018_instaviewer.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import unimelb.comp90018_instaviewer.R;
 import unimelb.comp90018_instaviewer.fragments.PhotoFragment;
@@ -14,6 +19,8 @@ import unimelb.comp90018_instaviewer.fragments.PhotoFragment;
 public class HomeActivity extends AppCompatActivity {
     private FragmentManager fm = getSupportFragmentManager();
     private PhotoFragment photoFragment = new PhotoFragment();
+    private static final String TAG = "HOME";
+    private FirebaseAuth mAuth;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,10 +46,17 @@ public class HomeActivity extends AppCompatActivity {
     };
 
     @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.i(TAG, "Logged in as: " + currentUser.getDisplayName());
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        mAuth = FirebaseAuth.getInstance();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
